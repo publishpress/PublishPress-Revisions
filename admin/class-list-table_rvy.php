39ae49d4528ca65f8ab355c0d26aea6402e0034f
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2019-12-18 23:30:37 -0500

    Revert "Enable Bulk Approval / Publishing"
    This reverts commit e212583946d0041c455105765f6df41e1963b207.

--- admin/class-list-table_rvy.php
+++ admin/class-list-table_rvy.php
@@ -607,6 +607,7 @@ class Revisionary_List_Table extends WP_Posts_List_Table {
 
 		$approval_potential = false;
 
+		/*
 		foreach(rvy_get_manageable_types() as $post_type) {
 			$type_obj = get_post_type_object($post_type);
 			if (!empty($current_user->allcaps[$type_obj->cap->edit_published_posts])) {
@@ -614,6 +615,7 @@ class Revisionary_List_Table extends WP_Posts_List_Table {
 				break;
 			}
 		}
+		*/
 
 		if ($approval_potential = apply_filters('revisionary_bulk_action_approval', $approval_potential)) {
 			$actions['approve'] = __('Approve', 'revisionary');
