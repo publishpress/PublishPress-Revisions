21401176eb662d3f60eca26728f3de7a9f7f8f89
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2019-12-19 02:17:32 -0500

    JReviews plugin: Post preview did not show unsaved changes

--- rvy_init.php
+++ rvy_init.php
@@ -21,7 +21,7 @@ add_action('init', 'rvy_set_notification_buffer_cron');
 add_action('rvy_mail_buffer_hook', 'rvy_send_buffered_mail' );
 add_filter('cron_schedules', 'rvy_mail_buffer_cron_interval');
 
-if (defined('JREVIEW_ROOT') && !empty($_REQUEST['preview']) && empty($_REQUEST['preview_id']) && empty($_REQUEST['thumbnail_id'])) {
+if (defined('JREVIEWS_ROOT') && !empty($_REQUEST['preview']) && empty($_REQUEST['preview_id']) && empty($_REQUEST['thumbnail_id'])) {
 	require_once('compat_rvy.php');
 	_rvy_jreviews_preview_compat();
 }
