519444f6136e302d3b62aa77b1c2b5ce70be764b
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2020-02-28 16:32:39 -0500

    Yoast SEO: Revision submission corrupted accented chars, emojis
    This Yoast SEO conflict was observed with the FAQ block

--- revision-creation_rvy.php
+++ revision-creation_rvy.php
@@ -220,7 +220,20 @@ class RevisionCreation {
                 $data['post_date'] = date( 'Y-m-d H:i:00', strtotime( $data['post_date'] ) );
             }
 
-            $data = wp_unslash( $data );
+			$emoji_fields = array( 'post_title', 'post_content', 'post_excerpt' );
+
+			foreach ( $emoji_fields as $emoji_field ) {
+				if ( isset( $data[ $emoji_field ] ) ) {
+					$charset = $wpdb->get_col_charset( $wpdb->posts, $emoji_field );
+					if ( 'utf8' === $charset ) {
+						$data[ $emoji_field ] = wp_encode_emoji( $data[ $emoji_field ] );
+					}
+				}
+			}
+
+			if (defined('RVY_REVISION_CREATION_DO_UNSLASH')) {
+            	$data = wp_unslash( $data );
+			}
 
             $revision_id = $this->create_revision($data, $postarr);
             if (!is_scalar($revision_id)) { // update_post_data() returns array or object on update abandon / failure
