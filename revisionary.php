203e3d0628a11094fe87597e3ed81c7ff558e0c3
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2020-11-05 12:05:25 -0500

    Release 2.4.3

--- revisionary.php
+++ revisionary.php
@@ -5,7 +5,7 @@
  * Description: Maintain published content with teamwork and precision using the Revisions model to submit, approve and schedule changes.
  * Author: PublishPress
  * Author URI: https://publishpress.com
- * Version: 2.4.2
+ * Version: 2.4.3
  * Text Domain: revisionary
  * Domain Path: /languages/
  * Min WP Version: 4.9.7
@@ -96,7 +96,7 @@ define('REVISIONARY_FILE', __FILE__);
 // register these functions before any early exits so normal activation/deactivation can still run with RS_DEBUG
 register_activation_hook(__FILE__, function() 
 	{
-		$current_version = '2.4.2';
+		$current_version = '2.4.3';
 
 		$last_ver = get_option('revisionary_last_version');
 
@@ -172,7 +172,7 @@ add_action(
 			return;
 		}
 
-		define('REVISIONARY_VERSION', '2.4.2');
+		define('REVISIONARY_VERSION', '2.4.3');
 
 		if ( ! defined( 'RVY_VERSION' ) ) {
 			define( 'RVY_VERSION', REVISIONARY_VERSION );  // back compat
