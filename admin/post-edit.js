a12b71fbc63f2cd1a64b1025510dca169cdc3c9f
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2021-03-04 14:57:47 -0500

    Classic Editor javascript: remove debug console output

--- admin/post-edit.js
+++ admin/post-edit.js
@@ -2,7 +2,9 @@ jQuery(document).ready( function($) {
 	$('#publish').show();
 	
 	$('#misc-publishing-actions div.num-revisions').contents().filter(function() {
-     if ( typeof(this.nodeValue) != 'undefined' ){ console.debug(this.nodeValue);} return ( this.nodeType == 3 && ( typeof(this.nodeValue) != 'undefined' ) && this.nodeValue.indexOf("Revisions") != -1 ); }).wrap('<span class="rev-caption"></span>');
+		//if ( typeof(this.nodeValue) != 'undefined' ){ console.debug(this.nodeValue);} 
+		return ( this.nodeType == 3 && ( typeof(this.nodeValue) != 'undefined' ) && this.nodeValue.indexOf("Revisions") != -1 ); 
+	}).wrap('<span class="rev-caption"></span>');
 	 
 	//$('#misc-publishing-actions span.rev-caption').html( rvyPostEdit.pubHistoryCaption + '&nbsp;' );
 	
