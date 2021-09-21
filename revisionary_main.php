215ae90eed9244e8c26ffb25382ed67389ee9321
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2021-09-21 01:09:11 -0400

    Remove functions moved to class Utils

--- revisionary_main.php
+++ revisionary_main.php
@@ -915,100 +915,6 @@ class Revisionary
 		return $rvy_creation->flt_create_scheduled_rev( $data, $post_arr );
 	}
 
-	/**
-     * Returns true if is a beta or stable version of WP 5.
-     *
-     * @return bool
-     */
-    public function isWp5()
-    {
-        global $wp_version;
-
-        return version_compare($wp_version, '5.0', '>=') || substr($wp_version, 0, 2) === '5.';
-    }
-	
-	/**
-	 * Based on Edit Flow's \Block_Editor_Compatible::should_apply_compat method.
-	 *
-	 * @return bool
-	 */
-	function isBlockEditorActive() {
-		// Check if PP Custom Post Statuses lower than v2.4 is installed. It disables Gutenberg.
-		if ( defined('PPS_VERSION') && version_compare(PPS_VERSION, '2.4-beta', '<') ) {
-			return false;
-		}
-
-		if (class_exists('Classic_Editor')) {
-			if (isset($_REQUEST['classic-editor__forget']) && (isset($_REQUEST['classic']) || isset($_REQUEST['classic-editor']))) {
-				return false;
-			} elseif (isset($_REQUEST['classic-editor__forget']) && !isset($_REQUEST['classic']) && !isset($_REQUEST['classic-editor'])) {
-				return true;
-			} elseif (get_option('classic-editor-allow-users') === 'allow') {
-				if ($post_id = rvy_detect_post_id()) {
-					$which = get_post_meta( $post_id, 'classic-editor-remember', true );
-
-					if ('block-editor' == $which) {
-						return true;
-					} elseif ('classic-editor' == $which) {
-						return false;
-					}
-				}
-			}
-		}
-
-		$pluginsState = array(
-			'classic-editor' => class_exists( 'Classic_Editor' ), // is_plugin_active('classic-editor/classic-editor.php'),
-			'gutenberg'      => function_exists( 'the_gutenberg_project' ), //is_plugin_active('gutenberg/gutenberg.php'),
-			'gutenberg-ramp' => class_exists('Gutenberg_Ramp'),
-		);
-
-		if ( ! $postType = rvy_detect_post_type() ) {
-			$postType = 'page';
-		}
-		
-		if ( $post_type_obj = get_post_type_object( $postType ) ) {
-			if ( empty( $post_type_obj->show_in_rest ) ) {
-				return false;
-			}
-		}
-
-		$conditions = array();
-
-		/**
-		 * 5.0:
-		 *
-		 * Classic editor either disabled or enabled (either via an option or with GET argument).
-		 * It's a hairy conditional :(
-		 */
-		// phpcs:ignore WordPress.VIP.SuperGlobalInputUsage.AccessDetected, WordPress.Security.NonceVerification.NoNonceVerification
-		$conditions[] = ($this->isWp5() || $pluginsState['gutenberg'])
-						&& ! $pluginsState['classic-editor']
-						&& ! $pluginsState['gutenberg-ramp']
-						&& apply_filters('use_block_editor_for_post_type', true, $postType, PHP_INT_MAX);
-
-		$conditions[] = $this->isWp5()
-                        && $pluginsState['classic-editor']
-                        && (get_option('classic-editor-replace') === 'block'
-                            && ! isset($_GET['classic-editor__forget']));
-
-        $conditions[] = $this->isWp5()
-                        && $pluginsState['classic-editor']
-                        && (get_option('classic-editor-replace') === 'classic'
-                            && isset($_GET['classic-editor__forget']));
-
-		$conditions[] = $pluginsState['gutenberg-ramp'] 
-						&& apply_filters('use_block_editor_for_post', true, get_post(rvy_detect_post_id()), PHP_INT_MAX);
-
-		// Returns true if at least one condition is true.
-		return count(
-				   array_filter($conditions,
-					   function ($c) {
-						   return (bool)$c;
-					   }
-				   )
-			   ) > 0;
-	}
-
 	function do_notifications( $notification_type, $status, $post_arr, $args ) {
 		global $rvy_workflow_ui;
 		if ( ! isset( $rvy_workflow_ui ) ) {
@@ -1040,27 +946,6 @@ class Revisionary
 		return $count;
 	}
 
-	public function isRESTurl() {
-		static $arr_url;
-	
-		if (!isset($arr_url)) {
-			$arr_url = parse_url(get_option('siteurl'));
-		}
-	
-		if ($arr_url) {
-			$path = isset($arr_url['path']) ? $arr_url['path'] : '';
-	
-			if (0 === strpos($_SERVER['REQUEST_URI'], $path . '/wp-json/oembed/')) {
-				return false;	
-			}
-	
-			if (0 === strpos($_SERVER['REQUEST_URI'], $path . '/wp-json/')) {
-				return true;
-			}
-		}
-	
-		return false;
-	}
 
 	function flt_custom_permalinks_query($query) {
 		global $wpdb;
