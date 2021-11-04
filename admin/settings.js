ce0bb3c5f34e6d208a2af1e01bdeeff06ddae913
Valentín García <htmgarcia@gmail.com>
2021-11-04 11:18:46 -0600

    Remove non required JS

--- admin/settings.js
+++ admin/settings.js
@@ -12,24 +12,4 @@ jQuery(document).ready(function ($) {
         $('table[id^="ppr-"]').hide();
         $(panel).show();
     });
-
-    var pprTab = 'ppr-tab-0';
-
-    if (typeof pprSettings != 'undefined' && typeof pprSettings.tab != 'undefined') {
-       pprTab = pprSettings.tab;
-       $('#publishpress-revisions-settings-tabs a[href="#' + pprTab + '"]').click();
-    }
-
-    var $hiddenFields = $('input[id^="ppr-tab-"]');
-
-    $hiddenFields.each(function () {
-        var $this = $(this);
-        var $wrapper = $this.next('table');
-        $wrapper.attr('id', $this.attr('id'));
-        $this.remove();
-
-        if ($wrapper.attr('id') !== pprTab) {
-            $wrapper.hide();
-        }
-    });
 });
