3469a872b7d1b7bbdc577b5579fc5889eda7002c
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2022-03-17 11:36:04 -0400

    Retroactive commit: Display Edit button after revision submission
    This was added in conjunction with the post editor UI "Edit" button display following revision creation. It is actually dysfunctional and not needed here since the revision is already being edited.

--- admin/rvy_revision-classic-edit.js
+++ admin/rvy_revision-classic-edit.js
@@ -6,8 +6,10 @@ if(rvyObjEdit[rvyObjEdit.currentStatus+'ActionCaption']){$(refSelector).after('<
 +'<div class="revision-created-wrapper" style="display: none; margin: 8px 0 0 2px">'
 +'<span class="revision-approve revision-created" style="color:green">'
 +rvyObjEdit[rvyObjEdit.currentStatus+'CompletedCaption']+'</span> '
-+'&nbsp;<a href="'+rvyObjEdit[rvyObjEdit.currentStatus+'CompletedURL']+'" class="revision-edit" target="_blank">'
++'&nbsp;<a href="'+rvyObjEdit[rvyObjEdit.currentStatus+'CompletedURL']+'" class="revision-preview" target="_blank">'
 +rvyObjEdit[rvyObjEdit.currentStatus+'CompletedLinkCaption']+'</a>'
++'&nbsp;<a href="'+rvyObjEdit[rvyObjEdit.currentStatus+'CompletedEditURL']+'" class="revision-edit" target="_blank">'
++rvyObjEdit[rvyObjEdit.currentStatus+'CompletedEditLinkCaption']+'</a>'
 +'</div>'
 +'</div> <br /><br />');}
 $('.edit-post-post-schedule__toggle').after('<button class="components-button is-tertiary post-schedule-footnote" disabled>'+rvyObjEdit.onApprovalCaption+'</button>');if(rvyObjEdit[rvyObjEdit.currentStatus+'DeletionURL']){$('a.submitdelete').attr('href',rvyObjEdit[rvyObjEdit.currentStatus+'DeletionURL']);}
@@ -15,6 +17,6 @@ $('#publish').hide();$('#save-post').val(rvyObjEdit.updateCaption);if(rvyObjEdit
 var RvyUIInterval=setInterval(RvySubmissionUI,100);$(document).on('click','a.save-timestamp, a.cancel-timestamp',function(){wp.autosave.server.triggerSave();});function RvyGetRandomInt(max){return Math.floor(Math.random()*max);}
 $(document).on('click','div.postbox-container',function(){$('a.revision-approve').attr('disabled','disabled');});$(document).on('click','a.revision-approve',function(){if($('a.revision-approve').attr('disabled')){return;}
 $('a.revision-approve').attr('disabled','disabled');if(wp.autosave.server.postChanged()){wp.autosave.server.triggerSave();var approvalDelay=250;}else{var approvalDelay=1;}
-if(!rvyObjEdit[rvyObjEdit.currentStatus+'ActionURL']){var revisionaryCreateDone=function(){$('a.revision-approve').hide();$('.revision-created-wrapper, .revision-created').show();rvyObjEdit.currentStatus='pending';$('#post-status-display').html(rvyObjEdit[rvyObjEdit.currentStatus+'StatusCaption']);$('a.revision-edit').attr('href',rvyObjEdit[rvyObjEdit.currentStatus+'CompletedURL']).show();}
+if(!rvyObjEdit[rvyObjEdit.currentStatus+'ActionURL']){var revisionaryCreateDone=function(){$('a.revision-approve').hide();$('.revision-created-wrapper, .revision-created').show();rvyObjEdit.currentStatus='pending';$('#post-status-display').html(rvyObjEdit[rvyObjEdit.currentStatus+'StatusCaption']);$('a.revision-preview').attr('href',rvyObjEdit[rvyObjEdit.currentStatus+'CompletedURL']).show();$('a.revision-edit').attr('href',rvyObjEdit[rvyObjEdit.currentStatus+'CompletedEditURL']).show();}
 var revisionaryCreateError=function(data,txtStatus){$('div.rvy-creation-ui').html(rvyObjEdit[rvyObjEdit.currentStatus+'ErrorCaption']);}
 var tmoSubmit=setInterval(function(){if(!wp.autosave.server.postChanged()){var data={'rvy_ajax_field':rvyObjEdit[rvyObjEdit.currentStatus+'AjaxField'],'rvy_ajax_value':rvyObjEdit.postID,'nc':RvyGetRandomInt(99999999)};$.ajax({url:rvyObjEdit.ajaxurl,data:data,dataType:"html",success:revisionaryCreateDone,error:revisionaryCreateError});clearInterval(tmoSubmit);}},approvalDelay);}else{var tmoApproval=setInterval(function(){if(!wp.autosave.server.postChanged()){window.location=rvyObjEdit[rvyObjEdit.currentStatus+'ActionURL'];clearInterval(tmoApproval);}},approvalDelay);return false;}});$(document).on('click','#post-body-content *, #content_ifr *, #wp-content-editor-container *, #tinymce *, #submitpost, span.revision-created',function(){$('.revision-created-wrapper, .revision-created').hide();$('a.revision-approve').html(rvyObjEdit[rvyObjEdit.currentStatus+'ActionCaption']).show().removeAttr('disabled');});});
