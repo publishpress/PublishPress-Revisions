b6f3bb40d0e1e4c7fcb7727bd513953e410902e6
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2022-03-17 16:14:53 -0400

    Scheduled revisions not included in dashboard Publishing Soon list
    Fixes #657

--- admin/admin-dashboard_rvy.php
+++ admin/admin-dashboard_rvy.php
@@ -1,5 +1,8 @@
 <?php
 class RevisionaryDashboard {
+	public function __construct() {
+	}
+
 	public function recentPostsQueryArgs($query_args) {
 		add_filter('posts_clauses_request', [&$this, 'fltDashboardQueryClauses']);
 
@@ -12,6 +15,10 @@ class RevisionaryDashboard {
 		$types = array_keys($revisionary->enabled_post_types);
 		$post_types_csv = implode( "','", array_map('sanitize_key', $types));
 		$clauses['where'] = str_replace( "$wpdb->posts.post_type = 'post'", "$wpdb->posts.post_type IN ('$post_types_csv')", $clauses['where'] );
+
+		$clauses['where'] = str_replace( "$wpdb->posts.post_status = 'future'", "($wpdb->posts.post_status = 'future' OR ($wpdb->posts.post_status = 'pending' AND $wpdb->posts.post_mime_type = 'future-revision'))", $clauses['where'] );
+
+		$revisionary->is_revisions_query = false;
 
 		remove_filter('posts_clauses_request', [&$this, 'fltDashboardQueryClauses']);
 		return $clauses;
