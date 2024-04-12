fbd1b45259eb42b40da8490d08810b1022a10585
Kevin Behrens <43488774+agapetry@users.noreply.github.com>
2024-04-12 01:51:11 -0400

    Extra precaution: ignore _rvy_has_revisions postmeta flag saved to a revision

--- rvy_init-functions.php
+++ rvy_init-functions.php
@@ -749,7 +749,8 @@ function revisionary_refresh_revision_flags($published_post_id = 0, $args = []) 
 	$revision_status_csv = implode("','", array_map('sanitize_key', $revision_statuses));
 
 	$query = "SELECT r.comment_count FROM $wpdb->posts r INNER JOIN $wpdb->posts p ON r.comment_count = p.ID"
-	. " WHERE p.post_status IN ('$status_csv') AND r.post_status IN ('$revision_base_status_csv') AND r.post_mime_type IN ('$revision_status_csv')";
+	. " WHERE p.post_status IN ('$status_csv') AND r.post_status IN ('$revision_base_status_csv')"
+	. " AND r.post_mime_type IN ('$revision_status_csv') AND p.post_mime_type NOT IN ('$revision_status_csv')";
 
 	if ($published_post_id) {
 		$query = $wpdb->prepare("$query AND p.ID = %d", $published_post_id);
